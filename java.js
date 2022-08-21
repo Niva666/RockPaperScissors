ac29762fa186ee9878d1d237ebe251cf95c2c9bf
Niva66 <njiva66@gmail.com>
2022-08-21 13:22:28 +0200

    This is the game with ternary's!

--- java.js
+++ java.js
@@ -8,44 +8,23 @@ let computer = Math.floor(Math.random()*10);
 console.log(computer);
 
 let choice;
-if (computer <=3) {choice="rock"}
-    else if (computer <=6 , computer >=4) {choice="paper"}
-    else if (computer > 6) {choice = "scissors"}
-    else {"Fuck this"}
+computer <=3 ? choice="rock" 
+: computer <=6 && computer >=4 ? choice="paper"
+: computer > 6 ? choice = "scissors"
+: "dick"
 console.log(choice);
+
 
 let winner;
 let w1="player wins";
 let w2="computer wins";
 let w0="tie";
 
-if (player === choice) {winner=w0}
-else if (player === "rock") {
-    if (choice === "paper") {
-        winner = w2
-    }
-    else {winner = w1  }
-}
-else if (player === "rock") {
-    if (choice === "scissors") {
-        winner = w1
-    }
-    else {winner = w2}
-}
-else if (player === "paper") {
-        if (choice === "scissors") {
-            winner = w2
-        }
-        else {winner = w1}
-    }
-else if (player === "scissors") {
-        if (choice === "rock") {
-            winner = w2
-        }
-        else {winner = w1}
-
-}
+player === choice ? winner=w0
+: player === "rock" && choice === "paper" ? winner = w2 
+: player === "paper" && choice === "scissors" ? winner = w2 
+: player === "scissors" && choice === "rock" ? winner = w2
+: winner = w1;
 console.log(winner);
 }
-
 a();
