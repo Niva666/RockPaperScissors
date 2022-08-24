1851fa98595868fb9f9e33128d4dfb0c74c36bda
Niva66 <njiva66@gmail.com>
2022-08-24 19:37:50 +0200

    The game with functions, without winner count;

--- java.js
+++ java.js
@@ -1,47 +1,68 @@
-let playerGame = confirm("Shall we play Rock,Paper,Scissors??")
-
-if (playerGame) {
-    for (i = 1; i <= 5;i++) {
-        alert("round " + i);
-    let playerChoice = prompt("Eneter Rock,Paper or Scissors...???");
-
-    if (playerChoice) {
-        let playerOne = playerChoice.trim().toLowerCase();
-            if(playerOne === "rock" 
-            || playerOne === "paper" 
-            || playerOne === "scissors") 
-            {   console.log(playerOne)
-                let computer = Math.floor(Math.random()*10);
-
-                let choice;
+function getComputerChoice () {
+    let choice;
+        let computer = Math.floor(Math.random()*10);
                 computer <=3 ? choice="rock" 
                 : computer <=6 && computer >=4 ? choice="paper"
                 : computer > 6 ? choice = "scissors"
                 : "dick"
-                alert(choice);
-
-                let winner;
-                let w1="player wins";
-                let w2="computer wins";
-                let w0="tie";
-
-                playerOne === choice ? winner = w0
-                : playerOne === "rock" && choice === "paper" ? winner = w2 
-                : playerOne === "paper" && choice === "scissors" ? winner = w2 
-                : playerOne === "scissors" && choice === "rock" ? winner = w2
-                : winner = w1;
-                alert(winner);
+                alert("Computer choice: " + choice);
+                return(choice);
+}
+function getPlayerChoice () {
 
 
-                
-
-                let playAgain= confirm("Play Again");
-                playAgain ? location.reload () : alert("Thanks for playing!");
-                }
-            else{alert("You didnt enter Rock, Paper or Scissors, Dude! This is a joke."
-             + "ON YOU!!!");}
+    let playerChoice = prompt("Eneter Rock,Paper or Scissors...???");
+    
+    if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    {
+        if(playerOne === "rock" 
+        || playerOne === "paper" 
+        || playerOne === "scissors") 
+        {
+        return(playerChoice);}
+        else {alert("Didnt enter value!")} 
     }
-    else {alert("I guess you really wanted to waste your Time!?")}
-} }  
- else {alert("Ok, why did you click on this Programm then," + 
-        "do something else then, dont waist my time then!!!")}
+    }
+    else {alert("You like wasting Time")}
+} 
+function playRound (playerSelection,computerSelection) {     
+
+playerSelection = getPlayerChoice();
+computerSelection = getComputerChoice();
+
+let winner;
+let w0 = ("Tie Game");
+let w1 = ("Player is the winner");
+let w2 = ("Computer is the winner");
+let userscore=0;
+let compscore=0;
+
+playerSelection === computerSelection ? winner = w0
+: playerSelection === "rock" && computerSelection === "scissors" ? winner = w1
+: playerSelection === "paper" && computerSelection === "rock" ? winner = w1
+: playerSelection === "scissors" && computerSelection === "paper" ? winner = w1
+: winner = w2;
+alert(winner);
+
+}
+function playGame() {
+
+
+let winner;
+let w0 = ("Tie Game");
+let w1 = ("Player is the winner");
+let w2 = ("Computer is the winner");
+let userscore=0;
+let compscore=0;
+let letsPlay = confirm("Play a game?")
+if(letsPlay){  
+{for (let i = 1; i <= 5; i++) {
+alert (`Round : ${i}`);
+playRound();
+}
+}}
+else{alert("Thanks")
+}
+}
+playGame();
