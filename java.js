4cb64db8166564cfaac3c9dbcd7e568a2ad856c6
Niva666 <njiva66@gmail.com>
2022-10-10 14:00:06 +0200

    new UI, the best i ca do for now

--- java.js
+++ java.js
@@ -31,35 +31,59 @@ yes.addEventListener('click' , revealSigns);
 
 let userscore = 0;
 let compscore = 0;
+let tiescore = 0;
+let playerChoise;
+let computerChoice;
 
+
+function playerTurn () {
+    sign.forEach((sign) => {
+        sign.addEventListener("click", function () {   
+            playerChoise = sign.textContent;
+            bro.textContent = "Player Chose: " + playerChoise ; 
+            return(playerChoise);
+        })
+    })
+}
+
+function computerTurn () {
+    let computer = ["rock","paper","scissors"];
+    computerChoice = computer[Math.floor(Math.random()*3)];
+    bro2.innerHTML = `Computer Chose: ${computerChoice}`;
+    return(computerChoice);
+    
+}
+
+function playRound () {
+
+    if(playerChoise===computerChoice){winner.innerHTML="Tie", tie.innerHTML= 'Ties: '+ (tiescore+=1)}
+    else if (playerChoise==='rock'&& computerChoice==='scissors'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' + (userscore+=1)}
+    else if (playerChoise==='paper'&& computerChoice==='rock'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' +(userscore+=1)}
+    else if (playerChoise==='scissors'&& computerChoice==='paper'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' +(userscore+=1)}
+    else{winner.innerHTML="Computer won the Round",cs.innerHTML='Computer Score: ' + (compscore+=1)}
+}
+
+function points () {
+
+            if(userscore===5) {gameWinner.innerHTML=("YOU WON!!!!YOU GO GET YOUR DREAMS")}
+            else if (compscore===5) {gameWinner.innerHTML=("GAME OVER")}
+            else if(tiescore===10){gameWinner.innerHTML=("Tie Game")}
+
+}
 function Game () {
     
+    
     sign.forEach((sign) => {
-        sign.addEventListener("click", function () {    
-            const playerChoise = sign.id
-            console.log('PC:'+ playerChoise)
-            bro.textContent = "Player Chose: " + playerChoise ; 
-            //Computer Choise
-
-            let computer = ["rock","paper","scissors"];
-            const compChoise = computer[Math.floor(Math.random()*3)];
-            console.log('CC:'+compChoise);
-            bro2.innerHTML = `Computer Chose: ${compChoise}`;
+        sign.addEventListener("click", function () {   
             
-           
-            if(playerChoise===compChoise){winner.innerHTML="Tie", tie.innerHTML= "No Points"}
-            else if (playerChoise==='rock'&& compChoise==='scissors'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' + (userscore+=1)}
-            else if (playerChoise==='paper'&& compChoise==='rock'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' +(userscore+=1)}
-            else if (playerChoise==='scissors'&& compChoise==='paper'){winner.innerHTML="Player won the Round",ps.innerHTML='Player Score: ' +(userscore+=1)}
-            else{winner.innerHTML="Computer won the Round",cs.innerHTML='Computer Score: ' + (compscore+=1)}
-            
+            playerTurn();
+            computerTurn ();
+            playRound ();
 
-            if(userscore>compscore) {gameWinner.innerHTML=("YOU WON!!!!YOU GO GET YOUR DREAMS")}
-            else if (userscore<compscore) {gameWinner.innerHTML=("GAME OVER")}
-            else{""}
-            //
+            points();
+        })
 
-        })})}
+        })}
     
 
 
